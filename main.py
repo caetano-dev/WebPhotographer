e943fc7a53a662669f4ca0dfc99940d3d3abee3d
drull1000 <pcaetanop@protonmail.com>
2021-06-24 22:32:21 -0300

    Starting the image function

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import telebot
 import screenshot
-bot = telebot.TeleBot("API CODE HERE", parse_mode=None)
+import asyncio
+import os
+
+bot = telebot.TeleBot("API KEY GOES HERE", parse_mode=None)
+
 
 @bot.message_handler(commands=["hello"])
 def greet(message):
-	bot.reply_to((message),"what's up?")
+    print("hello")
+    bot.reply_to((message), "what's up?")
 
-@bot.message_handler(commands=["ufrrj"])
-def greet(message):
-	bot.send_message((message.chat.id),"I'll check it")
-	screenshot()
+
+@bot.message_handler(commands=["website"])
+async def website(message):
+    asyncio.get_event_loop().run_until_complete(screenshot())
+    bot.send_message((message.chat.id), "I'll check it")
+    bot.send_photo((message.chat.id), photo=open("./webScreenshot.png", "rb"))
+    os.remove("./webScreenshot.png")
 
 bot.polling()
