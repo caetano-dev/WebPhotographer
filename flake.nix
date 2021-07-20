89538440ca7b8d594016ecba58dc48e8b17d774c
ImGabe <gabrielpmonte@hotmail.com>
2021-07-20 10:27:02 -0300

    update flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,5 @@
-# {
-#   description = "My flake";
-
-#   inputs = {
-#     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
-#     utils.url = "github:numtide/flake-utils";
-#   };
-
-#   outputs = { self, nixpkgs, utils }: (utils.lib.eachSystem ["x86_64-linux" ] (system: rec {
-
-#     packages = {
-#       pythonEnv = nixpkgs.legacyPackages.${system}.python3.withPackages(ps: with ps; [ selenium python-telegram-bot python-dotenv autopep8 ]);
-#     };
-
-#     defaultPackage = packages.pythonEnv; # If you want to juist build the environment
-#     devShell = packages.pythonEnv.env; # We need .env in order to use `nix develop`
-#   }));
-# }
-
 {
-  description = "telegramBot";
+  description = "WebPhotographer";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
@@ -31,7 +12,7 @@
       with import nixpkgs { system = "x86_64-linux"; };
 
       pkgs.python3Packages.buildPythonPackage rec {
-        name = "telegramBot";
+        name = "WebPhotographer";
         src = ./.;
         propagatedBuildInputs = with pkgs.python3Packages; [
           selenium
